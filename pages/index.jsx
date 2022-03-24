58b1ece5ba68329cade37d74fa86717531ed9da6
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-03-24 13:06:40 +0530

    home page complete key

--- pages/index.jsx
+++ pages/index.jsx
@@ -67,9 +67,9 @@ export default function Home() {
 
           <div className={styles.daoListContainer}>
             {
-              [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map(() => {
+              [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20].map((ele) => {
                 return (
-                  <DaoCard />
+                  <DaoCard key={'c'+ele} />
                 )
               })
             }
